14cbc4c1d652f58784c5e33bb53fb4ad58e24264
akevit <akevit@10.0.0.15>
2014-03-04 21:05:11 -0800

    v1 complete

---
+++ src/xbmc/tools/XBMCUpdate_Main.java
@@ -0,0 +1,24 @@
+/*
+ * XBMCUpdate_Main.java
+ */ 
+
+package xbmc.tools;
+
+/**
+ *
+ * @author Jens Bodal
+ */
+
+public class XBMCUpdate_Main {
+    
+    public static void main(String[] args) {
+        if (args.length == 4) {
+            String username = args[0];
+            String password = args[1];
+            String host = args[2];
+            String port = args[3];
+            XBMCUpdate update = new XBMCUpdate(username, password, host, port);
+            update.sendUpdateRequest();
+        }
+    }
+}
