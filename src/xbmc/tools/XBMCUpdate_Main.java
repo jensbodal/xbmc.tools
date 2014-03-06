c19758c3637f40dc9871c2a9f5a31327e9a58f15
akevit <akevit@10.0.0.15>
2014-03-05 18:28:25 -0800

    Updated command line

--- src/xbmc/tools/XBMCUpdate_Main.java
+++ src/xbmc/tools/XBMCUpdate_Main.java
@@ -11,8 +11,8 @@ public class XBMCUpdate_Main {
 
     public static void main(String[] args) {
         if (args.length > 0) {
-            if (args[0].equals("-u")) {
-                if (args.length == 5) {
+            if (args[0].equals("-update")) {
+                if (args.length == 6) {
                     String username = args[1];
                     String password = args[2];
                     String host = args[3];
@@ -20,16 +20,17 @@ public class XBMCUpdate_Main {
                     XBMCUpdate update = new XBMCUpdate(username, password, host, port);
                     System.out.printf("Updating: %s %n", update.getURL());
                     update.sendUpdateRequest();
-                } 
-                else {
+                } else {
                     StringBuilder invalidParams = new StringBuilder();
                     invalidParams.append("Please run this program as follows: \n");
                     invalidParams.append("\tjava -jar XBMC_Tools.jar ");
+                    invalidParams.append("-update ");
                     invalidParams.append("\"username\" ");
                     invalidParams.append("\"password\" ");
                     invalidParams.append("\"host\" ");
                     invalidParams.append("\"port\"\n");
                     invalidParams.append("\te.g. java -jar XBMC_Tools.jar ");
+                    invalidParams.append("-update ");
                     invalidParams.append("\"xbmc\" ");
                     invalidParams.append("\"xbmc\" ");
                     invalidParams.append("\"10.0.0.151\" ");
