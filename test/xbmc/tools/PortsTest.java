21cf548f11a00d794ef3b06a5b94976c3ff15de9
Jens Bodal <jensbodal@gmail.com>
2014-03-10 20:41:45 -0700

    Experimenting with JUNit Tests

--- test/xbmc/tools/PortsTest.java
+++ test/xbmc/tools/PortsTest.java
@@ -9,40 +9,44 @@ import org.junit.Test;
  * @author jensb
  */
 public class PortsTest {
+
     Emailer emailer = new Emailer();
     private final String testDomain = "portquiz.net";
-    
-    @Ignore
+
     @Test
     public void testPort25() {
         boolean expectedResult = true;
-        boolean result = emailer.testPort(testDomain, 25);
+        boolean result = false;
+        long waitTime = System.currentTimeMillis() + 1000;
+        while (System.currentTimeMillis() < waitTime) {
+            result = emailer.testPort(testDomain, 25);
+        }
         assertEquals("Your ISP is likely blocking sending mail on outgoing "
-                    + "port 25", expectedResult, result);
+                + "port 25", expectedResult, result);
     }
-    
-    @Test(timeout=1000)
+
+    @Test(timeout = 1000)
     public void testPort587() {
         boolean expectedResult = true;
         boolean result = emailer.testPort(testDomain, 587);
         assertEquals(expectedResult, result);
     }
-    
-    @Test(timeout=1000)
+
+    @Test(timeout = 1000)
     public void testPort81() {
         boolean expectedResult = true;
         boolean result = emailer.testPort(testDomain, 81);
         assertEquals(expectedResult, result);
     }
-    
-    @Test(timeout=1000)
+
+    @Test(timeout = 1000)
     public void gmailSMTP() {
         boolean expectedResult = true;
         boolean result = emailer.testPort("smtp.gmail.com", 587);
         assertEquals(expectedResult, result);
     }
-    
-    @Test(timeout=1000)
+
+    @Test(timeout = 1000)
     public void gmail25() {
         boolean expectedResult = true;
         boolean result = emailer.testPort("smtp.gmail.com", 25);
